60dcb422d1d0b85b0d7d996b888ac87230b4a028
Dmitrii Kolikov <kolikov.dmitrii@gmail.com>
2020-12-27 00:25:57 +0600

    Add day1 task1,2,3,4,5,6

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        for(int value = 0; value < 10; value++ ) {
+            System.out.println("JAVA");
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 3;
+        for(int value = 1; value < 10; value++) {
+            System.out.println(value + " * 3 = " + value * k);
+        }
     }
 }
