5e65a9e2548619cd3ed79e60b1decdd822a9ba92
Dmitrii Kolikov <kolikov.dmitrii@gmail.com>
2021-01-02 21:52:57 +0700

    Solution day8 task1,2

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,9 +1,27 @@
 package day8;
 
-
 public class Task1 {
     public static void main(String[] args) {
+        long start1 = System.currentTimeMillis();
+        String string = "";
+        for(int i =0; i < 20000; i ++) {
+           string = string + i + " ";
+        }
+        System.out.println(string);
+        long finish1 = System.currentTimeMillis();
+        long timeConsumedMillis1 = finish1 - start1;
+        System.out.println(timeConsumedMillis1);
 
+        System.out.println();
 
+        long start2 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder("");
+        for(int i =0; i < 20000; i ++) {
+          sb.append(i).append(" ");  //for me
+        }
+        System.out.println(sb);
+        long finish2 = System.currentTimeMillis();
+        long timeConsumedMillis2 = finish2 - start2;
+        System.out.println(timeConsumedMillis2);
     }
-}
+}    //String 1059 ms     StringBuilder 79 ms
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -1,7 +1,10 @@
 package day8;
 
+
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boeing 737-800", 2012, 395, 41140);
+        System.out.println(airplane);
 
     }
 }
