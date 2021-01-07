d29b23f69aed6e9ae4aa6eaaac61ca6a7c42e935
Dmitrii Kolikov <kolikov.dmitrii@gmail.com>
2021-01-07 10:21:38 +0700

    Correct day12 task2

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -6,19 +6,15 @@ import java.util.List;
 public class Task2 {
     public static void main(String[] args) {
         List<Integer> evenNumbers = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            if (i % 2 == 0) {
+        for (int i = 0; i < 500; i++) {
+            if (i < 30 && i % 2 == 0) {
                 evenNumbers.add(i);
+                System.out.print(i + " ");
+            }
+            else if (i >= 300 && i % 2 == 0) {
+                evenNumbers.add(i);
+                System.out.print(i + " ");
             }
         }
-        for (int i = 300; i < 500; i++) {
-            if (i % 2 == 0) {
-                evenNumbers.add(i);
-            }
-        }
-        for (Integer x: evenNumbers) {
-            System.out.print(x + " ");
-        }
-
     }
 }
