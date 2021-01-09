d2fd5a9d803047d73cb2070cb7c258bb99d4ce4f
Dmitrii Kolikov <kolikov.dmitrii@gmail.com>
2021-01-09 17:08:05 +0700

    Solution day15 task1

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,30 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import au.com.bytecode.opencsv.CSVReader;
 
-    }
-}
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("C:\\Users\\Dmitry\\Desktop\\JavaMarathon\\JavaMarathon2020\\src\\main\\resources\\shoes.csv"), ';');
+        String[] nextLine;
+        File fac = new File("C:\\Users\\Dmitry\\Desktop\\JavaMarathon\\JavaMarathon2020\\src\\main\\resources\\Check");
+        PrintWriter pw = new PrintWriter(fac);
+
+        List<String[]> s = new ArrayList<>();
+        while ((nextLine = reader.readNext()) != null) {
+            if (nextLine[2].equals("0")) {
+                pw.println(Arrays.toString(nextLine));
+            }
+                }
+        pw.close();
+        }
+            }
+
+
+
+
+
